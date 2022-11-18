09ee1a799038a3901d557672af742998eb7fcdb1
Victor S <victor.skarerverket@gmail.com>
2022-11-18 05:26:54 +0100

    added error handling to functions

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 import webbrowser
 import requests
 from requests.exceptions import HTTPError
-import json
 
 
 def getChannels():
@@ -19,7 +18,6 @@ def getChannels():
         return channels
 
 
-#WIP
 def chooseStation(channels):
     # List Channels
     stationCounter = 0
@@ -27,25 +25,41 @@ def chooseStation(channels):
         print(f'{stationCounter} - {channel["name"]}')
         stationCounter = stationCounter + 1
     val = input("Pick a station: ")
-    # accept only integers between 0 and stationCounter
+    try:
+        val = int(val)
+    except Exception as err:
+        print(f'Invalid station option: {err}')
+        return
+    if val < 0 or val > stationCounter:
+        print("Invalid station option")
+        return
     stationOptions(val, channels)
 
 
 #WIP
 def stationOptions(val, channels):
     # add other options or data you'd like from chosen station.
+    # TODO: Implement menu similar to main.menuOptions()
+    listLastSong(val, channels)
     playStation(val, channels)
 
 
 def playStation(stationVal, channels):
     channelUrl = channels[(int(stationVal))]['liveaudio']['url']
     webbrowser.open_new_tab(channelUrl)
-    listLastAndNextSong(stationVal, channels)
 
 
-def listLastAndNextSong(stationVal, channels):
+def listLastSong(stationVal, channels):
     channelId = channels[(int(stationVal))]['id']
-    currentSongRequest = requests.get(f'http://api.sr.se/api/v2/playlists/rightnow?channelid={channelId}&format=json')
+    try:
+        currentSongRequest = requests.get(f'http://api.sr.se/api/v2/playlists/rightnow?channelid={channelId}&format=json')
+        currentSongRequest.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error: {http_err}')
+    except Exception as err:
+        print(f'Other error: {err}')
     songData = currentSongRequest.json()
-    print(f"Previous Song \nArtist: {songData['playlist']['previoussong']['artist']} - Song: {songData['playlist']['previoussong']['title']}")
-    print(f"Next Song \nArtist: {songData['playlist']['nextsong']['artist']} - Song: {songData['playlist']['nextsong']['title']}")
+    print(f"Previous Song: {songData['playlist']['previoussong']['artist']} - {songData['playlist']['previoussong']['title']}")
+
+
+#Considering making a function for get requests that does try/catch.
