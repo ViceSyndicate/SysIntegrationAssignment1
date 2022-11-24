b4e7bc28a8fc2d1d367d9cce16afe5a2247ce0b1
adoracionbronzini <90825658+adoracionbronzini@users.noreply.github.com>
2022-11-25 00:30:49 +0100

    My first commit

--- main.py
+++ main.py
@@ -3,19 +3,26 @@
 #https://api.sr.se/api/documentation/v2/metoder/kanaler.html
 #https://api.sr.se/api/documentation/v2/metoder/kanaler.html
 import functions
-
+import functions1
 
 def menu_options():
     print("----------")
+    print("0 - Exit")
     print("1 - List Stations")
     print("2 - Search for Program")
-    print("0 - Exit")
+    print("3 - Extra Programs")
     print("----------")
+
     user_input = input()
-    if user_input == "1":
+    if user_input == "0":
+        print("Exiting Program")
+        exit()
+    elif user_input == "1":
         functions.get_channels()
-    if user_input == "2":
+    elif user_input == "2":
         functions.search_for_program()
+    elif user_input == "3":
+        functions1.my_menu()
     # Other function options
     return user_input
 
